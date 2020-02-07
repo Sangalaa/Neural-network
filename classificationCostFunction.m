2ab6feeb2e21bf6fbc1fdba9c2c5ddbb0b98b6c8
Adam Bublavy <bublavy.adam@gmail.com>
2020-02-07 11:02:51 +0100

    Added forwardPropagation to classificationCostFunction and cost calculation added

--- classificationCostFunction.m
+++ classificationCostFunction.m
@@ -6,5 +6,16 @@ function [J, grad] = classificationCostFunction(model, X, y, lambda)
   grad = [];
   
   num_labels = model.layers(end).units;
+  
+  layers = forwardPropagation(model, X);
+  
+  predictions = layers.a_layers(end).layer;
+  
+  vectorized_predictions = arrayfun(@(x) [1:num_labels] == x, y, 'UniformOutput', false);
+  vectorized_predictions = cell2mat(vectorized_predictions);
+  
+  predictions_errors = sum(vectorized_predictions .* log(predictions) + (1 - vectorized_predictions) .* log(1 - predictions));
 
+  J = sum(predictions_errors) / -m;
+  
 endfunction
